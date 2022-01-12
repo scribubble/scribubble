22e5515ca45de0dfe8b86929765a607cc9c0cb44
kyechan99 <kyechan99@gmail.com>
2022-01-12 15:01:59 +0900

    Design: zoom bar

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -191,3 +191,25 @@ export const SquareButton = (props) => {
         </RoundButton>
     );
 };
+
+export const PlusButton = (props) => {
+    return (
+        <RoundedButton onClick={props.onClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <line x1="12" y1="5" x2="12" y2="19"></line>
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+            </svg>
+        </RoundedButton>
+    );
+};
+export const MinusButton = (props) => {
+    return (
+        <RoundedButton onClick={props.onClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+            </svg>
+        </RoundedButton>
+    );
+};
